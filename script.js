c8c9668e8c0b38e258f1f281d0f64b028bbae479
Tan Yi Jun <tanyijun6@gmail.com>
2023-12-23 17:26:43 +0800

    Initialize sketchboard protoype

--- script.js
+++ script.js
@@ -1,61 +1,33 @@
-function createGridContainer(gridSize) {
-    const mainContainer = document.querySelector("#main-container");
-    const gridContainer = document.createElement("div");
-    gridContainer.setAttribute('id', 'grid-container');
+function createGrid() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    return grid;
+}
+
+function createGridColumn(column) {
+    const gridColumn = document.createElement('div');
+    gridColumn.classList.add('column');
+
+    for (let i = 0; i < column; i++) {
+        const grid = createGrid();
+        gridColumn.appendChild(grid);
+    }
+
+    return gridColumn;
+}
+
+function createSketchboard(row, column) {
+    const sketchboard = document.createElement('div');
+    sketchboard.id = 'sketchboard';
+
+    for (let i = 0; i < row; i++) {
+        const gridColumn = createGridColumn(column);
+        sketchboard.appendChild(gridColumn);
+    }
     
-    for (let i = 0; i < gridSize; i++) {
-        const gridRow = document.createElement('div');
-        gridRow.setAttribute('class', 'grid-row');
-        for (let i = 0; i < gridSize; i++) {
-            const gridItem = createGridItem();
-            gridRow.appendChild(gridItem);
-        }
-        gridContainer.appendChild(gridRow);
-    }
-
-    gridContainer.addEventListener('dragstart', e => e.preventDefault()); // prevent drag
-    mainContainer.appendChild(gridContainer);
+    return sketchboard;
 }
 
-function createGridItem() {
-    const gridItem = document.createElement('div');
-    gridItem.setAttribute('class', 'grid-item');
-    gridItem.addEventListener('mousedown', setColor);
-    gridItem.addEventListener('mouseover', setColor);
-    return gridItem;
-}
-
-function setColor(e) {
-    if (e.type === 'mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = 'black';
-}
-
-function removeCurrentGrid() {
-    const currentGrid = document.getElementById('grid-container');
-    currentGrid.remove();
-}
-
-function changeGridSize() {
-    let newSize = prompt('Enter a new size (1~100)', 16);
-    while (newSize > 100 || newSize < 1) {
-        newSize = prompt('Please enter a size between 1~100, try again:', 16);
-    }
-    removeCurrentGrid();
-    createGridContainer(newSize);
-}
-
-function createGridSizeButton() {
-    const mainContainer = document.querySelector('#main-container');
-    const gridSizeButton = document.createElement('button');
-    gridSizeButton.setAttribute('id', 'grid-size-button');
-    gridSizeButton.innerText = "Change grid size";
-    gridSizeButton.addEventListener('click', changeGridSize);
-    mainContainer.appendChild(gridSizeButton);
-}
-
-createGridSizeButton();
-createGridContainer(16);
-
-let mouseDown = 0;
-document.body.onmousedown = () => mouseDown = 1;
-document.body.onmouseup = () => mouseDown = 0;
+const container = document.querySelector('#container');
+const sketchboard = createSketchboard(16, 16);
+container.appendChild(sketchboard);
